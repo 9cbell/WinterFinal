29ddba8ac9e764a41df7d6624ae4e1770c661ea3
9cbell <31480938+9cbell@users.noreply.github.com>
2017-12-13 10:09:21 -0600

    Add files via upload

--- ChargingDock.js
+++ ChargingDock.js
@@ -3,20 +3,29 @@ function ChargingDock(){
 
 //Instance variables
     this.ports = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]; //finish from instructions
-    this.leds = ["red", "yellow", "green"]; //finish from instructions
+    this.leds = ["red", "red", "red", "red", "red","red", "red", "red"]; //finish from instructions
 
 
 //Instance Fucntions
     this.plug = function(dvc){
         for(let p=0; p<=8; p++){
-             if(this.ports[p] == "red"){
+             if(this.leds[p] == "red"){
              }
             }
-            if(dvc >)
+            if(this.juice > .99){
+                this.leds = "green";
+            }
+            if (this.juice < .99){
+                this.leds = "yellow";
+            }
     };
 
     this.unplug = function(dvcIdx){
-        //type in here
+        for(d=0; d<= this.leds.length; d++){
+            if(this.ports[d] == "yellow" || "green"){
+                
+            }
+        }
     };
 
     this.chargeAll = function(min){
